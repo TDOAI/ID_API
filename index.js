78c8709bc41f5e865979c175bb367eff41463a8c
unknown <C:\Users\Admin\AppData\Roaming\The Bat!>
2022-10-21 16:23:06 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+import compression from 'compression';
+import dotenv from 'dotenv';
+dotenv.config();
+import Fastify from 'fastify';
+const fastify = Fastify({
+  logger: false,
+});
+await fastify.register(
+    import('@fastify/express'),
+    { threshold: 2048 }
+);
+import { fetch_movie } from './slider.js';
+
+fastify.use(compression());
+fastify.get('/', async (request, reply) => {
+    const message = "WELCOME TO FLIXNODE API";
+    reply.type('application/json').code(200).send(JSON.stringify(message));
+    console.log(message);
+});
+
+fastify.get('/slider', async (request, reply) => {
+    const message = await fetch_movie();
+    reply.type('application/json').code(200).send(JSON.stringify(message));
+});
+  
+  // Run the server!
+fastify.listen({ port: 3000 }, (err, address) => {
+    if (err) throw err
+    console.log(`Server is now listening on ${address}`)
+});
+
---
+++ slider.js
@@ -0,0 +1,44 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import axios from "axios";
+import { MongoClient } from 'mongodb';
+import pkg from 'fast-sort';
+const { sort } = pkg;
+
+const base_url = process.env.BASE_URL;
+const api_key = process.env.API_KEY;
+const movie_url_1 = `${base_url}trending/movie/day?api_key=${api_key}&page=1`;
+const movie_url_2 = `${base_url}trending/movie/day?api_key=${api_key}&page=2`;
+const db = process.env.DB;
+
+async function fetch_movie() {
+    const arr = []
+    const client = new MongoClient(db);
+    await client.connect();
+    const collection = await client.db().collection('movies');
+    const page_1 = await axios.get(movie_url_1);
+    const res_1 = await page_1.data.results;
+    const page_2 = await axios.get(movie_url_2);
+    const res_2 = await page_2.data.results;
+    const res = await [...new Set([...res_1, ...res_2])]
+    const promises = await (res || []).map(async ob => {
+        const ObFromDb = await collection.findOne({ tmdb_id: ob.id.toString() })
+        if (ObFromDb !== null) {
+            arr.push(ob);
+        }
+    });
+    await Promise.all(promises);
+    const top_arr = [];
+    for (let i=0; i < arr.length; i++) {
+        if (arr[i].id > 7) {
+            top_arr.push(arr[i]);
+        }
+    }
+    const sorted = sort(top_arr).desc(en => en.vote_average);
+    const sliced = sorted.splice(0, 10);
+    client.close();
+    return sliced
+}
+
+
+export { fetch_movie }
